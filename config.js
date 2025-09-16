cfb5a0ac502e00ab9e34d318c1894654ba9a1492
DominicDev <s0l0estudi4r@gmail.com>
2025-09-16 11:12:11 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'FLEETWOOD MAC';
+export let nameSongs = [
+	"LITTLE LIES", /*1*/
+	"THE CHAIN ", /*2*/
+	"DREAMS", /*3*/
+	"RHIANNON", /*4*/
+	"LANDSLIDE", /*5*/
+	"GIPSY", /*6*/
+	"GO YOUR OWN WAY ", /*7*/
+	"SEVEN WONDERS", /*8*/
+	"DON'T STOP", /*9*/
+	"SARA", /*10*/
+	"GOLD DUST WOMAN", /*11*/
+	"SILVER SPRINGS", /*12*/
+	"YOU MAKE LOVING FUN", /*13*/
+	"SAY YOU LOVE ME", /*14*/
+	"OVER MY HEAD", /*15*/
+	"CRYSTAL", /*16*/
+	"WORLD TURNING", /*17*/
+	"SUGAR DADDY", /*18*/
+	"EVERYWHERE", /*19*/
+	"AS LONG AS YOU FOLLOW", /*20*/
+]
